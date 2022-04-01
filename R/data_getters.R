f9a37f1fb92e5048c19ffdfa6675e61a4e3eec39
Stoyel <Quentin.Stoyel@dfo-mpo.gc.ca>
2022-04-01 14:25:37 -0300

    add all base data_getter functions, pass check package

--- R/data_getters.R
+++ R/data_getters.R
@@ -29,12 +29,16 @@ download_res <- function(resId) {
     warning(paste("Resource corresponding to that resId not found:", e))})
 
   dataDir <- file.path(tempdir(), "dataDir")
+  if (dir.exists(dataDir)){
+    unlink(dataDir, recursive=TRUE)
+  }
   dataDirSucess <- dir.create(dataDir, showWarnings = FALSE)
   dataZip <- file.path(dataDir, "temp.zip")
 
-  download.file(res$url, dataZip)
-  utils::unzip(dataZip, exdir = dataDir)
-
+  utils::download.file(res$url, dataZip)
+  if (is_zip(dataZip)){
+    utils::unzip(dataZip, exdir = dataDir)
+  }
   return(dataDir)
 }
 
@@ -45,7 +49,9 @@ download_res <- function(resId) {
 #' multiple geometry types, running sf::st_make_valid, cropping to an sf if provided
 #' and setting the crs.
 #'
-#' @param resId String of the resource id associated with the open data resource
+#' @param in_sf Sf object to be cleaned.
+#' @param crop_sf Sf object to crop the spatial data to
+#' @param crs Coordinate reference system used for output sf
 #' @return File path of the directory
 sf_clean <- function(in_sf, crop_sf, crs) {
   if  (inherits(sf::st_geometry(in_sf), "sfc_GEOMETRY")) {
@@ -73,6 +79,27 @@ get_gdb_layer <- function(gdbLayer, gdbPath) {
   out_sf <- sf::st_read(gdbPath, stringsAsFactors = FALSE, layer = gdbLayer)
   out_sf$geometry <- sf::st_geometry(out_sf)
   return(out_sf)
+}
+
+#' Read a Tif file
+#'
+#' This is a helper function to read in a tif file to stars object.
+#'
+#' @param tifPath File path of the tif file
+#' @param crop_sf Sf object to crop the spatial data to
+#' @param crs Coordinate reference system used for output sf
+#' @return stars object
+get_star_tif <- function(tifPath, crs, crop_sf) {
+  out_star <- stars::read_stars(tifPath) %>%
+    sf::st_transform(crs=crs)
+  if (!is.null(crop_sf)) {
+    if (length(sf::st_intersection(sf::st_bbox(out_star), crop_sf)) > 0) {
+      out_star <- sf::st_crop(out_star, crop_sf)
+    } else {
+      out_star <- NULL
+    }
+  }
+  return(out_star)
 }
 
 #' Read an esri layer
@@ -105,6 +132,21 @@ get_esri_layer <- function(esriLayer, esriUrl, where, geometry) {
   return(out_sf)
 }
 
+#' Check if a file is a zip file
+#'
+#' Helper function that sees if a file can be unziped or not.
+#'
+#' @param filepath filepath of possible zip file
+#' @return boolean TRUE if file is zipped, else FALSE
+is_zip <- function(filepath){
+  result <- tryCatch({
+    utils::unzip(filepath, list = TRUE)
+    return(TRUE)
+  }, error = function(e){
+    return(FALSE)
+  })
+}
+
 
 #' Read an open data record
 #'
@@ -117,7 +159,7 @@ get_esri_layer <- function(esriLayer, esriUrl, where, geometry) {
 #' @describeIn getRes
 #'
 #' @param resId String of the resource id associated with the open data resource
-#' @param cropSf Sf object to crop the spatial data too
+#' @param crop_sf Sf object to crop the spatial data to
 #' @param crs Coordinate reference system used for output sf
 #' @return An sf object
 #' @export
@@ -129,7 +171,8 @@ get_shp_res <- function(resId, crop_sf=NULL, crs=4326) {
   if (length(shpFile) > 0) {
     tryCatch({out_sf <- sf::st_read(shpFile, stringsAsFactors = FALSE)},
              error = function(e) {
-               stop(paste("Error reading shapefile:"))
+               stop(paste("Error reading shapefile:", e,
+                          "Downloaded files are at: ", dataDir), sep="\n")
                })
   } else {
     stop(paste("No Shapefile downloaded. Downloaded files are at: ", dataDir))
@@ -144,7 +187,7 @@ get_shp_res <- function(resId, crop_sf=NULL, crs=4326) {
 #' @describeIn getRes
 #'
 #' @param resId String of the resource id associated with the open data resource
-#' @param cropSf Sf object to crop the spatial data too
+#' @param crop_sf Sf object to crop the spatial data too
 #' @param crs Coordinate reference system used for output sf
 #' @param gdbLayer string name of layer to extract, if null all layers will be
 #' extracted
@@ -153,7 +196,7 @@ get_shp_res <- function(resId, crop_sf=NULL, crs=4326) {
 get_gdb_res <- function(resId, crop_sf=NULL, crs=4326, gdbLayer=NULL) {
   dataDir <- download_res(resId)
 
-  gdbDir <- list.files(tempDir, recursive=TRUE, pattern="\\.gdb$",
+  gdbDir <- list.files(dataDir, recursive=TRUE, pattern="\\.gdb$",
                        include.dirs	= TRUE, full.names = TRUE)
   if (length(gdbDir) > 0) {
     tryCatch({
@@ -175,6 +218,7 @@ get_gdb_res <- function(resId, crop_sf=NULL, crs=4326, gdbLayer=NULL) {
   # cleanup
   sfList <- sapply(sfList, sf_clean, crop_sf=crop_sf, crs=crs, simplify=FALSE,
                    USE.NAMES=TRUE)
+  unlink(dataDir, recursive = TRUE)
   return(sfList)
 }
 
@@ -182,7 +226,85 @@ get_gdb_res <- function(resId, crop_sf=NULL, crs=4326, gdbLayer=NULL) {
 #' @describeIn getRes
 #'
 #' @param resId String of the resource id associated with the open data resource
-#' @param cropSf Sf object to crop the spatial data too
+#' @param crop_sf Sf object to crop the spatial data too
+#' @param crs Coordinate reference system used for output sf
+#' @param tifFile string name of tif file to be extracted
+#' @param getSf Boolean. If TRUE, function will return list of sf, may
+#' SIGNIFICANTLY increase processing time and resources. Default is FALSE
+#' @return A named list of stars/sf objects depending on getSf flag, named
+#' according to filename.
+#' @export
+get_tif_res <- function(resId, crop_sf=NULL, crs=4326, tifFile=NULL,
+                        getSf=FALSE) {
+  dataDir <- download_res(resId)
+
+  if (!is.null(tifFile)) {
+    tifRasterFiles <- list.files(dataDir, recursive = TRUE, pattern =
+                                   paste("\\",tifFile, "$", sep = ""),
+                                 full.names = TRUE)
+    tifRasterNames <- list.files(dataDir, recursive = TRUE, pattern =
+                                   paste("\\",tifFile, "$", sep = ""),
+                                 full.names = FALSE)
+  } else {
+    tifRasterFiles <- list.files(dataDir, recursive = TRUE, pattern = "\\.tif$",
+                                 include.dirs	= TRUE, full.names = TRUE)
+    tifRasterNames <- list.files(dataDir, recursive = TRUE, pattern = "\\.tif$",
+                                 full.names = FALSE)
+  }
+  if (length(tifRasterFiles) > 0) {
+    tifRasterNames <- gsub(".tif", "", tifRasterNames)
+    outList <- lapply(tifRasterFiles, get_star_tif, crs=crs, crop_sf=crop_sf)
+    names(outList) <- tifRasterNames
+
+    if (getSf){
+      outList <- sapply(outList, sf::st_as_sf, simplify=FALSE,
+                       USE.NAMES=TRUE)
+    }
+  } else {
+    stop(paste("No matching Tif files downloaded. Downloaded files are at: ",
+               dataDir))
+  }
+  # cleanup
+  unlink(dataDir, recursive = TRUE)
+  return(outList)
+}
+
+#' @describeIn getRes
+#'
+#' @param resId String of the resource id associated with the open data resource
+#' @param fileName String name of file to save
+#' @param savePath Full Path file should be saved to.
+#' @return Directory containing the downloaded files.
+#' @export
+get_data_res <- function(resId, fileName=NULL, savePath=NULL) {
+  dataDir <- download_res(resId)
+
+  if (!is.null(fileName)) {
+    filePaths <- list.files(dataDir, recursive = TRUE, pattern =
+                                   paste("\\", fileName, "$", sep = ""),
+                                 full.names = TRUE)
+  } else {
+    filePaths <- list.files(dataDir, recursive = TRUE,
+                                 include.dirs	= FALSE, full.names = TRUE)
+  }
+
+  if (!is.null(savePath)) {
+    if (!dir.exists(savePath)) {
+      dir.create(savePath, showWarnings = FALSE)
+    }
+    file.copy(filePaths, savePath, recursive=TRUE)
+    unlink(dataDir, recursive = TRUE)
+    return(savePath)
+  } else {
+    return(dataDir)
+  }
+}
+
+
+#' @describeIn getRes
+#'
+#' @param resId String of the resource id associated with the open data resource
+#' @param crop_sf Sf object to crop the spatial data too
 #' @param crs Coordinate reference system used for output sf
 #' @param esriLayer String layer number of the layer to be extracted
 #' @param esriWhere where clause to filter esri results
@@ -192,7 +314,7 @@ get_esri_res <- function(resId, crop_sf=NULL, crs=4326, esriLayer=NULL, esriWher
   ckanr::ckanr_setup(url="https://open.canada.ca/data")
   tryCatch({res <- ckanr::resource_show(resId)}, error = function(e) {
     warning(paste("Resource corresponding to that resId not found:", e))})
-  if (!is.null(layer)) {
+  if (!is.null(esriLayer)) {
     sfList = list(esriLayer=get_esri_layer(esriLayer = esriLayer,
                                            esriUrl = res$url,
                                            where = esriWhere,
@@ -216,42 +338,3 @@ get_esri_res <- function(resId, crop_sf=NULL, crs=4326, esriLayer=NULL, esriWher
                    USE.NAMES=TRUE)
   return(sfList)
 }
-
-
-
-# --------------download_extract_validate_sf-----------------
-# Function that downloads and processes a file into an sf object
-#
-# Inputs:
-# 1. zipUrl: URL string of zipped file containing the spatial data
-# 2. region_sf: Sf object to crop the output sf too
-# 3. gdbLayer: if the file is a geodatabase, name of the layer to be read
-# 4. tifFile: if the unzipped files are Tifs, name of the file to be used
-# 5. returnRaster: if False, rasters will be converted into vector files
-#
-# Outputs:
-# 1 .out_sf: output sf object containing spatial data
-download_extract_validate_sf <- function(zipUrl, region_sf = NULL,
-                                         gdbLayer = NULL, tifFile = NULL,
-                                         returnRaster=FALSE) {
-  if (FALSE) {
-  tifRasterFile <- list.files(tempDir, recursive=TRUE, pattern = paste("\\",tifFile, "$", sep = ""),
-                              include.dirs	= TRUE, full.names = TRUE)
-  } else if (length(tifRasterFile) > 0) {
-    tifRaster <- raster(tifRasterFile)
-    if (returnRaster){
-      if (!is.null(region_sf)) {
-        tifRaster <- projectRaster(tifRaster, crs=crs(region_sf))
-        if (length(st_intersection(st_as_sfc(st_bbox(tifRaster)), region_sf)) > 0) {
-          tifRaster <- crop(tifRaster, region_sf)
-        } else {
-          tifRaster <- NULL
-        }
-      }
-      return(tifRaster)
-    }
-    out_sf <- stars::st_as_stars(tifRaster) %>% st_as_sf()
-  }
-}
-
-
